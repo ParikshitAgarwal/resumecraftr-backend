7b48ee5063426ec1c2b08b4defe9a9aa4cb4bf36
ParikshitAgarwal <parikshitagarwal134@gmail.com>
2024-12-25 14:39:32 +0530

    Updated code for better performance

--- src/index.js
+++ src/index.js
@@ -11,15 +11,8 @@ app.use(cors({
 }));
 app.use(bodyParser.json());
 
-const getChromeLauncherPath = async () => {
-  const chromeLauncher = await import('chrome-launcher');
-  const launcher = chromeLauncher.default.Launcher.getInstallations()
-  return launcher
-  console.log(launcher)
-}
 
 const doSomePuppeteerThings = async (jsonFormData, id) => {
-  //  const chromExecutablePath = await getChromeLauncherPath();
   console.log(process.env.CHROME_EXECUTABLE_PATH)
   let browser = null;
   const url = 'https://www.resumecraftr.in/resume-preview/' + id;
@@ -27,15 +20,6 @@ const doSomePuppeteerThings = async (jsonFormData, id) => {
     browser = await puppeteer.launch({
       args: [
         ...chromium.args,
-        '--disable-gpu',
-        '--disable-dev-shm-usage',
-        '--disable-setuid-sandbox',
-        '-no-first-run',
-        '--no-sandbox',
-        '--no-zygote',
-        '--deterministic-fetch',
-        '--disable-features=IsolateOrigins',
-        '--disable-site-isolation-trials'
       ],
       executablePath: process.env.CHROME_EXECUTABLE_PATH || await chromium.executablePath(),
       headless: chromium.headless,
@@ -44,21 +28,9 @@ const doSomePuppeteerThings = async (jsonFormData, id) => {
       formData: jsonFormData
     };
     await setDomainLocalStorage(browser, url, localStorage);
-    console.log("second")
     const page = await browser.newPage();
-    // do your actual puppeteer things now
 
-    await page.goto(url, {
-      waitUntil: ['networkidle0', 'load', 'domcontentloaded'],
-      timeout: 3000,
-
-    });
-
-    const divContent = await page.evaluate(() => {
-      const targetDiv = document.querySelector("#previewLayout");
-
-      return targetDiv ? targetDiv.textContent : 'Div not found!';
-    });
+    await page.goto(url);
 
 
     const pdfBuffer = await page.pdf({
@@ -76,24 +48,9 @@ const doSomePuppeteerThings = async (jsonFormData, id) => {
 };
 
 const setDomainLocalStorage = async (browser, url, values) => {
-  console.log("first")
   const page = await browser.newPage();
-  console.log("first2")
 
-  await page.setRequestInterception(true);
-  console.log("first3")
-
-  page.on('request', r => {
-    r.respond({
-      status: 200,
-      contentType: 'text/plain',
-      body: 'tweak me.',
-    });
-  });
-  await page.goto(url, {
-    waitUntil: ['networkidle0', 'load', 'domcontentloaded'],
-    timeout: 3000
-  });
+  await page.goto(url);
   await page.evaluate(values => {
     for (const key in values) {
       localStorage.setItem(key, values[key]);
@@ -110,36 +67,6 @@ app.post("/pdf", async (req, res) => {
 
   try {
     const pdfBuffer = await doSomePuppeteerThings(jsonFormData, id)
-    // Launch Puppeteer
-    // const browser = await puppeteer.launch({headless: false,args: ['--no-sandbox','--disable-setuid-sandbox']
-    // });
-    // const page = await browser.newPage();
-    // console.log("page:",page)
-    // console.log("browser:",browser)
-    // console.log("formData",formData)
-    // console.log(jsonFormData)
-    // Set the content of the page
-    // await page.setCookie([{'formDataa': jsonFormData}])
-
-    // await page.goto('http://localhost:3000/resume-preview', {
-    //     waitUntil: 'load',
-
-    //   });
-
-
-
-
-
-    // await page.setContent(html, { waitUntil: 'networkidle2' , format: 'A4', printBackground: true});
-
-
-    // Generate PDF
-    // const pdfBuffer = await page.pdf({
-    //     printBackground: true
-    //   });
-    // console.log(pdfBuffer)
-    // Close the browser
-    // await browser.close();
 
     // Send the PDF as a response
     res.set({
